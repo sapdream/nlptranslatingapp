8ad2f18e8065cee5b6d7016f6bc731c1c648f6c8
sapdream <156305094+sapdream@users.noreply.github.com>
2024-01-12 10:44:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import transformers
 import streamlit as st
 import time
+import transformers
+
+def load_models():
+    # English to Chinese
+    EN_ZH_MODEL = "Helsinki-NLP/opus-mt-en-zh"
+    en_zh_tokenizer = transformers.AutoTokenizer.from_pretrained(EN_ZH_MODEL)
+    en_zh_model = transformers.AutoModelForSeq2SeqLM.from_pretrained(EN_ZH_MODEL)
+    en_zh_translator = transformers.pipeline("text2text-generation", model=en_zh_model, tokenizer=en_zh_tokenizer, device=0)
+    # Chinese to English
+    ZH_EN_MODEL = "Helsinki-NLP/opus-mt-zh-en"
+    zh_en_tokenizer = transformers.AutoTokenizer.from_pretrained(ZH_EN_MODEL)
+    zh_en_model = transformers.AutoModelForSeq2SeqLM.from_pretrained(ZH_EN_MODEL)
+    zh_en_translator = transformers.pipeline("text2text-generation", model=zh_en_model, tokenizer=zh_en_tokenizer, device=0)
+    return en_zh_translator, zh_en_translator
+    
+en_zh_translator, zh_en_translator = load_models()
 
 @st.cache_resource
 def load_models():
